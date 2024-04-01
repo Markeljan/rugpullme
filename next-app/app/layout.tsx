172261c29a9c6a9f18a54e058870282a7a5b8022
Markeljan Sokoli <Markeljan19@gmail.com>
2024-04-01 16:10:42 -0400

    og image

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -8,6 +8,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Rug Pull Me",
   description: "Rug Pull Me",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://rugpullme.xyz/og-image.png",
+    title: "Rug Pull Me",
+    description: "Rug Pull Me meme",
+    images: [
+      {
+        url: "https://rugpullme.xyz/og-image.png",
+        alt: "Rug Pull Me",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
